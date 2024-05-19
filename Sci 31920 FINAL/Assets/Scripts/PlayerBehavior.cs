f72a60000f29c0a8620ce0bc5ce586d6edb7b0c4
coco <43858608+Amandal4012@users.noreply.github.com>
2024-05-18 21:31:05 -0400

    comment playerbehavior

--- Sci 31920 FINAL/Assets/Scripts/PlayerBehavior.cs
+++ Sci 31920 FINAL/Assets/Scripts/PlayerBehavior.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-
+    // movespeed = forward, backward
+    // rotatespeed = left, right
+    // v input is vertical axis input. h input is horizontal axis input
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
     private float _vInput;
@@ -15,27 +17,28 @@ public class PlayerBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // gain reference to rigidbody component of player to move it
         _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // get value of both vertical and horizontal axis and multiply by move/rotate speed to determine quickness of movement 
+        // detect when wasd is pressed and value of 1/-1 of W/S keys respectively multipled by move/rotate speed
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
 
-        /*
-        this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
-        */
     }
 
     void FixedUpdate()
     {
+        // move and rotate object for each next frame at a constant speed
+        // left/right rotation axis
         Vector3 rotation = Vector3.up * _hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
 
+        // direction annd speed the player will move throughout z axis 
         _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
     }
