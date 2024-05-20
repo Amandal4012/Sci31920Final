337cd6307d89d83babfa147bf2b6f1dd09f72122
coco <43858608+Amandal4012@users.noreply.github.com>
2024-05-19 21:09:39 -0400

    level 2 button pad

--- Sci 31920 FINAL/Assets/Scripts/level2.cs
+++ Sci 31920 FINAL/Assets/Scripts/level2.cs
@@ -4,41 +4,25 @@ using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class changeLevel : MonoBehaviour
+public class level2 : MonoBehaviour
 {
-   // int changelevel = GetSceneAt
+    // int changelevel = GetSceneAt
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerEnter(Collider myCollision)
     {
 
-        SceneManager.LoadScene("World 1-2");
-
-        /*
         SceneManager.LoadScene("World 1-3");
-
-        SceneManager.LoadScene("World 1-4");
-
-        SceneManager.LoadScene("World 2-1");
-
-        SceneManager.LoadScene("World 2-2");
-
-        SceneManager.LoadScene("World 2-3");
-
-        SceneManager.LoadScene("World 2-4");
-        */
     }
-        
-    
 }
