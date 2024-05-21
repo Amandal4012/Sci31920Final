ee0d4469bb59c7399548f38653b0dabd15c378bc
coco <43858608+Amandal4012@users.noreply.github.com>
2024-05-20 20:34:01 -0400

    comments

--- Sci 31920 FINAL/Assets/Scripts/CameraBehavior.cs
+++ Sci 31920 FINAL/Assets/Scripts/CameraBehavior.cs
@@ -7,21 +7,28 @@ public class CameraBehavior : MonoBehaviour
     
     public Vector3 CamOffset = new Vector3(0f, 1.2f, -2.6f);
     private Transform _target;
-
+  //  public bool alive;
     // Start is called before the first frame update
     void Start()
     {
         
         _target = GameObject.Find("Player").transform;
+       // bool alive = GetComponent<bool>();
+        
+        //alive = FindObjectOfType<PlayerBehavior>();
+       // Debug.Log(alive.aliveStatus);
+       //Debug.Log(alive.ToString());
     }
 
    
     void LateUpdate()
     {
 
-        
-        this.transform.position = _target.TransformPoint(CamOffset);
-        this.transform.LookAt(_target);
+       // if (aliveStatus)
+       // {
+            this.transform.position = _target.TransformPoint(CamOffset);
+            this.transform.LookAt(_target);
+        //}
     }
     // Update is called once per frame
     void Update()
--- Sci 31920 FINAL/Assets/Scripts/PlayerBehavior.cs
+++ Sci 31920 FINAL/Assets/Scripts/PlayerBehavior.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
@@ -11,6 +13,7 @@ public class PlayerBehavior : MonoBehaviour
     jump velocity = how high to jump
     _isJumping = detect if currently jumpig or not
     _col = the player collider 
+    _rb = allow to control player's body
     */
     public float MoveSpeed = 10f;
     public float RotateSpeed = 75f;
@@ -22,7 +25,7 @@ public class PlayerBehavior : MonoBehaviour
     private CapsuleCollider _col;
 
     private Rigidbody _rb;
-
+    //[SerializeField]  bool aliveStatus = true;
     public float DistanceToGround = 0.1f;
     public LayerMask GroundLayer;
     // Start is called before the first frame update
@@ -36,14 +39,16 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (aliveStatus)
+        {
+            // get value of both vertical and horizontal axis and multiply by move/rotate speed to determine quickness of movement 
+            // detect when wasd is pressed and value of 1/-1 of W/S keys respectively multipled by move/rotate speed
+            _vInput = Input.GetAxis("Vertical") * MoveSpeed;
+            _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
 
-        // get value of both vertical and horizontal axis and multiply by move/rotate speed to determine quickness of movement 
-        // detect when wasd is pressed and value of 1/-1 of W/S keys respectively multipled by move/rotate speed
-        _vInput = Input.GetAxis("Vertical") * MoveSpeed;
-        _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
-
-        // allows jump to occur once after holding space down
-        _isJumping |= Input.GetKeyDown(KeyCode.Space);
+            // allows jump to occur once after holding space down
+            _isJumping |= Input.GetKeyDown(KeyCode.Space);
+        }
 
     }
 
@@ -51,36 +56,62 @@ public class PlayerBehavior : MonoBehaviour
     {
         // move and rotate object for each next frame at a constant speed
         // left/right rotation axis
-        Vector3 rotation = Vector3.up * _hInput;
-        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
 
-        // direction annd speed the player will move throughout z axis 
-        _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
-        _rb.MoveRotation(_rb.rotation * angleRot);
-
-        // allow player to jump under forces like gravity and jump velocity
-        if (IsGrounded() && _isJumping)
+        if (aliveStatus)
         {
-            _rb.AddForce(Vector3.up * JumpVelocity,
-            ForceMode.Impulse);
+
+        
+            Vector3 rotation = Vector3.up * _hInput;
+            Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+            // direction annd speed the player will move throughout z axis 
+            _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
+            _rb.MoveRotation(_rb.rotation * angleRot);
+
+            // allow player to jump under forces like gravity and jump velocity
+            if (IsGrounded() && _isJumping)
+            {
+                _rb.AddForce(Vector3.up * JumpVelocity,
+                ForceMode.Impulse);
+            }
+            // if not jumping, state is at rest
+            _isJumping = false;
         }
-        // if not jumping, state is at rest
-        _isJumping = false;
-    }
-
-    // detect collision between collider and ground layer
-    private bool IsGrounded()
-    {
-        // store collider bottom position
-        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x,
-        _col.bounds.min.y, _col.bounds.center.z);
-
-        // check if collider is grounded based on closeness to ground
-        bool grounded = Physics.CheckCapsule(_col.bounds.center,
-        capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
-
-        return grounded;
-
 
     }
-}
+
+        // detect collision between collider and ground layer
+        bool IsGrounded()
+        {
+            // store collider bottom position
+            Vector3 capsuleBottom = new Vector3(_col.bounds.center.x,
+            _col.bounds.min.y, _col.bounds.center.z);
+
+            // check if collider is grounded based on closeness to ground
+            bool grounded = Physics.CheckCapsule(_col.bounds.center,
+            capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+
+            return grounded;
+
+
+        }
+    /*
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.name == "Mob")
+            {
+                Debug.Log("You died!");
+                aliveStatus = false;
+                Die();
+
+            }
+        }
+
+        void Die()
+        { 
+         Destroy(this.gameObject);
+        Time.timeScale = 0f;
+        Application.Quit();
+        }
+    */
+    }
