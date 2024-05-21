343f68e36e955a80e355fc7a6e06f67f62fcd5ac
coco <43858608+Amandal4012@users.noreply.github.com>
2024-05-20 22:40:38 -0400

    world 2.1

---
+++ Sci 31920 FINAL/Assets/Scripts/level change/level7.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level7 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider myCollision)
+    {
+        SceneManager.LoadScene("World 2-4");
+    }
+
+}
+
